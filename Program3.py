2e3c584a498bfb27c0324a4a413b1dfc1849460c
Oktavianuzzz <48722600+Oktavianuzzz@users.noreply.github.com>
2019-03-31 21:39:09 +0700

    Add files via upload

---
+++ Program3.py
@@ -0,0 +1,85 @@
+#DoNotRemake...!!!
+#Author -> Mr.5qu1dy
+
+import os
+import time
+
+os.system ('apt install figlet;apt install git;apt install python;apt install ruby;geminstall lolcat')
+os.system ('clear')
+os.system ('figlet Program3 | lolcat')
+time.sleep(1)
+opening='''
+#################################################
+#       Author  : Mr.5quidy                     #
+#       Team    : MOBM T34M                     #
+#                 R.B.A Team                    #
+#       S Thanks: All Admin & All Member        #
+#################################################
+'''
+
+print (opening)
+time.sleep(1)
+print ('Tools Version 1.6')
+time.sleep(1)
+print ('Selection Menu : ')
+time.sleep(1)
+menu='''
+[01] Install Tools Phising
+[02] Install Tools Spam WA
+[03] Install Tools Malicious
+[04] Install Tools Metasploit
+[05] MapSCII
+[06] Install Program1 (Tools Sebelum Ini)
+
+[00] Close
+'''
+print (menu)
+pilihan=input ('Enter Your Choice : ')
+if pilihan=='1' or pilihan=='01':
+   os.system ('clear')
+   time.sleep(0.2)
+   os.system ('figlet Phising | lolcat')
+   os.system ('apt update && apt upgrade;apt install git;apt install python2;git clone https://github.com/UndeadSec/SocialFish.git')
+   time.sleep(1)
+elif pilihan=='2' or pilihan=='02':
+     os.system ('clear')
+     time.sleep(0.2)
+     os.system ('figlet Tools Spam WA | lolcat')
+     os.system ('apt update && apt upgrade;apt install git;apt install php;git clone https://github.com/siputra12/prank')
+     time.sleep(1)
+elif pilihan=='3' or pilihan=='03':
+     os.system ('clear')
+     time.sleep(0.2)
+     os.system ('figlet Malicious | lolcat')
+     os.system ('apt update && apt upgrade;apt install git;apt install python2 -y;git clone https://github.com/Hider5/Malicious')
+     time.sleep(1)
+elif pilihan=='4' or pilihan=='04':
+     os.system ('clear')
+     time.sleep(0.2)
+     os.system ('figlet Metasploit | lolcat')
+     os.system ('apt update && apt upgrade;pkg install unstable-repo;pkg install metasploit')
+     time.sleep(1)
+elif pilihan=='5' or pilihan =='05':
+     os.system ('clear')
+     time.sleep(0.2)
+     os.system ('figlet MapSCII')
+     os.system ('telnet mapscii.me')
+     time.sleep(1)
+elif pilihan=='6' or pilihan=='06':
+     os.system ('clear')
+     time.sleep(0.2)
+     os.system ('figlet Program1 | lolcat')
+     os.system ('apt update && apt upgrade;apt install git;git clone https://github.com/Oktavianuzzz/ProgramNewbie')
+     time.sleep(1)
+elif pilihan=='0' or pilihan=='00':
+     print ('Terima Kasih Telah Download Tools Saya...!')
+     time.sleep(1)
+     print ('Follow IG saya @wwdzn._')
+     time.sleep(1)
+     print ('Disana Bakalan Ada Tutorial Tentang Termux..')
+     time.sleep(1)
+     print (' ')
+
+else:
+    time.sleep(1)
+    print ('3RR0R: Wrong Input...!!!')
